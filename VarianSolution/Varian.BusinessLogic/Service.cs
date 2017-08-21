d860e5059a6fdbdd79042e2b7b121ada741fdbde
Wilson Afonso <wilson.afonso@gmail.com>
2017-08-21 18:20:32 +0100

    First commit.

---
+++ VarianSolution/Varian.BusinessLogic/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Varian.BusinessLogic
+{
+    public class Service
+    {
+        /// <summary>
+        /// IntToRoman - Convert int to Roman number
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static string IntToRoman(int number)
+        {
+                return number.ToString();
+        }
+    }
+}
---
+++ VarianSolution/Varian.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Varian.BusinessLogic;
+
+namespace Varian.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine(Service.IntToRoman(n));
+            }
+        }
+    }
+}
