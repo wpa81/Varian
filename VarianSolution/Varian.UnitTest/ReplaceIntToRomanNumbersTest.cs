c299b58a878809fb47a0c446c06fedc45252ba57
Wilson Afonso <wilson.afonso@gmail.com>
2017-08-22 16:51:55 +0100

    Implementation of test cases for ReplaceIntToRomanNumber.

---
+++ VarianSolution/Varian.UnitTest/ReplaceIntToRomanNumbersTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Varian.BusinessLogic;
+using Varian.BusinessLogic.Model;
+
+namespace Varian.UnitTest
+{
+    [TestClass]
+    public class ReplaceIntToRomanNumbersTest
+    {
+        /// <summary>
+        /// TestCase1
+        /// </summary>
+        [TestMethod]
+        public void TestCase1()
+        {
+            // arrange  
+            var input = "Lorem ipsum 2 dolor sit amet.";
+            ReplaceIntToRomanNumberOutput output = null;
+
+            // act  
+            try
+            {
+                 output = Service.ReplaceIntToRomanNumber(input);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.IsNotNull(output);
+            Assert.AreEqual("Lorem ipsum II dolor sit amet.", output.NewString);
+            Assert.AreEqual(1, output.NumberOfReplacements);
+        }
+
+        /// <summary>
+        /// TestCase2
+        /// </summary>
+        [TestMethod]
+        public void TestCase2()
+        {
+            // arrange  
+            var input = "Consectetur 5 adipiscing elit 9.";
+            ReplaceIntToRomanNumberOutput output = null;
+
+            // act  
+            try
+            {
+                output = Service.ReplaceIntToRomanNumber(input);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.IsNotNull(output);
+            Assert.AreEqual("Consectetur V adipiscing elit IX.", output.NewString);
+            Assert.AreEqual(2, output.NumberOfReplacements);
+        }
+
+        /// <summary>
+        /// TestCase3
+        /// </summary>
+        [TestMethod]
+        public void TestCase3()
+        {
+            // arrange  
+            var input = "Ut enim quis nostrum 1904 qui.";
+            ReplaceIntToRomanNumberOutput output = null;
+
+            // act  
+            try
+            {
+                output = Service.ReplaceIntToRomanNumber(input);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.IsNotNull(output);
+            Assert.AreEqual("Ut enim quis nostrum M CM IV qui.", output.NewString);
+            Assert.AreEqual(1, output.NumberOfReplacements);
+        }
+    }
+}
