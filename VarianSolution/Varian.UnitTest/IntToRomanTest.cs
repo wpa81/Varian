bb59d3e51890c370206f17bfa8636cd64d73235f
Wilson Afonso <wilson.afonso@gmail.com>
2017-08-22 14:31:22 +0100

    Implementation of unit tests for IntToRoman.

---
+++ VarianSolution/Varian.UnitTest/IntToRomanTest.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Varian.BusinessLogic;
+
+namespace Varian.UnitTest
+{
+    [TestClass]
+    public class IntToRomanTest
+    {
+        /// <summary>
+        /// TestFourDigitsNumber - Test a four digits number
+        /// </summary>
+        [TestMethod]
+        public void TestFourDigitsNumber()
+        {
+            // arrange  
+            var number = 3999;
+            var output = string.Empty;
+
+            // act  
+            try
+            {
+                output = Service.IntToRoman(number);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.AreEqual("MMM CM XC IX", output);
+        }
+
+        /// <summary>
+        /// TestThreeDigitsNumber - Test a three digits number
+        /// </summary>
+        [TestMethod]
+        public void TestThreeDigitsNumber()
+        {
+            // arrange  
+            var number = 999;
+            var output = string.Empty;
+
+            // act  
+            try
+            {
+                output = Service.IntToRoman(number);
+            } catch(Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.AreEqual("CM XC IX", output);
+        }
+
+        /// <summary>
+        /// TestTwoDigitsNumber - Test a two digits number
+        /// </summary>
+        [TestMethod]
+        public void TestTwoDigitsNumber()
+        {
+            // arrange  
+            var number = 16;
+            var output = string.Empty;
+
+            // act  
+            try
+            {
+                output = Service.IntToRoman(number);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.AreEqual("X VI", output);
+        }
+
+        /// <summary>
+        /// TestOneDigitNumber - Test an one digit number
+        /// </summary>
+        [TestMethod]
+        public void TestOneDigitNumber()
+        {
+            // arrange  
+            var number = 3;
+            var output = string.Empty;
+
+            // act  
+            try
+            {
+                output = Service.IntToRoman(number);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+
+            // assert  
+            Assert.AreEqual("III", output);
+        }
+
+        /// <summary>
+        /// TestNumberLowerThanRange - Test a number lower than 1
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestNumberLowerThanRange()
+        {
+            // arrange  
+            var number = 0;
+            var output = string.Empty;
+
+            // act  
+            output = Service.IntToRoman(number);
+
+        }
+
+        /// <summary>
+        /// TestNumberGreaterThanRange - Test a number greater than 3999
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestNumberGreaterThanRange()
+        {
+            // arrange  
+            var number = 4999;
+            var output = string.Empty;
+
+            // act  
+            output = Service.IntToRoman(number);
+        }
+    }
+}
---
+++ VarianSolution/Varian.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Varian.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Varian.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f921d5d-6beb-4b4e-8adb-ba14fc0a1140")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
